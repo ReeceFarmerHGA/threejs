46a5c37b838443f4a03980db688a56832a5323be
ReeceFarmerHGA <Reece.Farmer@hgacreative.com>
2018-04-16 15:38:56 +0100

    Initial commit

---
+++ resources/views/three.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Three</title>
+        <script src="{{ asset('app.js') }}"></script>
+    </head>
+    <body>
+        threejs
+    </body>
+</html>
