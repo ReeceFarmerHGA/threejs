297ed397fa5ca474d1e0a885b4e4914ebb76ff11
ReeceFarmerHGA <Reece.Farmer@hgacreative.com>
2018-04-25 16:42:17 +0100

    spinning shape

--- resources/views/three.blade.php
+++ resources/views/three.blade.php
@@ -3,9 +3,74 @@
     <head>
         <meta charset="utf-8">
         <title>Three</title>
-        <script src="{{ asset('app.js') }}"></script>
+        <style media="screen">
+            body {
+                margin: 0px;
+            }
+            canvas,
+            body,
+            html {
+                height: 100% !important;
+                width: 100% !important;
+            }
+        </style>
     </head>
     <body>
-        threejs
+        <script src="{{ asset('js/app.js') }}"></script>
+
+        <script>
+        var camera, scene, renderer;
+        var geometry, material, mesh;
+
+        init();
+        animate();
+
+        function init() {
+
+            camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
+            camera.position.z = 1;
+
+            scene = new THREE.Scene();
+
+            geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+            material = new THREE.MeshNormalMaterial();
+
+            group = new THREE.Group(); //create an empty container
+
+
+            directions = ['x', 'y', 'z'];
+            for (i = 0; i <= 2; i++) {
+                for (var key in directions) {
+                    var direction = directions[key];
+                    var spacing = 0.15;
+                    thisMesh = new THREE.Mesh( geometry, material );
+                    thisMesh.position[direction] = spacing * i;
+                    group.add(thisMesh);
+
+                    thisMeshInvert = new THREE.Mesh( geometry, material );
+                    thisMeshInvert.position[direction] = (spacing * i) - ((spacing * i) * 2);
+                    group.add(thisMeshInvert);
+                }
+            }
+
+            renderer = new THREE.WebGLRenderer( { antialias: true } );
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.body.appendChild( renderer.domElement );
+
+            scene.add( group );
+
+        }
+
+        function animate() {
+
+            requestAnimationFrame( animate );
+
+            group.rotation.x += 0.05;
+            group.rotation.y += 0.05;
+
+            renderer.render( scene, camera );
+
+        }
+        </script>
     </body>
 </html>
